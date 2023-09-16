9ddde3037bbe6991fca368f09155dd5b1160e8cb
CjangCjengh <101577701+CjangCjengh@users.noreply.github.com>
2023-09-16 22:26:05 +0800

    fix path

--- pychunom/__init__.py
+++ pychunom/__init__.py
@@ -1,9 +1,11 @@
+import os
 import opencc
 
 
 __all__ = ['convert']
 
-converter = opencc.OpenCC('dict/chunom.json')
+_thisdir = os.path.dirname(os.path.abspath(__file__))
+converter = opencc.OpenCC(os.path.join(_thisdir, 'dict/chunom.json'))
 
 def convert(text: str) -> str:
     return converter.convert(text.lower().replace(' ','-')).replace('-','')
